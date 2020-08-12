32edce7e2e1c0de92228bf9735f0a494f80f0b7d
Nicholas Pease <me@nicholaspease.com>
2020-08-12 11:08:20 -0400

    API Somewhat working, token now reads from config

--- avwx.js
+++ avwx.js
@@ -1,15 +1,12 @@
-import config from 'keys.json'
-
-var apikey = config.avwx
 var baseurl = "https://avwx.rest/api/"
 
 function nearestStations(x,y) {
-    FetchPending = $.ajax({ url: baseurl + 'station/near/'+x+','+y+'?n=10&airport=true&reporting=true&format=json?token='+apikey,
+    FetchPending = $.ajax({ url: baseurl + 'station/near/'+x+','+y+'?n=10&airport=true&reporting=true&format=json?token='+AVWX,
                                     timeout: 5000,
                                     cache: false,
                                     dataType: 'json' });
         FetchPending.done(function(data) {
-            
-        }
+            document.getElementById("results").innerHTML = data
+        })
     
 }
