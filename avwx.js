71967cc366a522e8ef8bf9124d2298fccb04d323
LAX18 Development <gaminglax18@gmail.com>
2020-08-12 22:18:39 -0400

    API Key update to block CORS

--- avwx.js
+++ avwx.js
@@ -1,18 +1,13 @@
 var baseurl = "https://avwx.rest/api/"
 
 function nearestStations(x,y) {
-    FetchPending = $.ajax({ url: baseurl + 'metar/'+x+','+y+'?'+AVWX+'&format=json',
-                    beforeSend: function(request) { 
-                        request.setRequestHeader("Authority", AVWX);
-                    },
+    FetchPending = $.ajax({ url: baseurl + 'metar/'+x+','+y+'?token='+AVWX+'&format=json',
                                     timeout: 5000,
                                     cache: false,
                                     dataType: 'json',
-                                    headers: {
-                                        "Authorization":AVWX
-                                    }});
+                                    });
         FetchPending.done(function(data) {
-            document.getElementById("results").innerHTML = data
+            document.getElementById("results").innerHTML = "Station ID: "+data.station
         })
     
 }
