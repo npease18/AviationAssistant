b562ad74dc0734176bf449c34a56b5a50bf5e200
LAX18 Development <gaminglax18@gmail.com>
2020-08-12 21:11:41 -0400

    API Working

--- avwx.js
+++ avwx.js
@@ -1,10 +1,13 @@
 var baseurl = "https://avwx.rest/api/"
 
 function nearestStations(x,y) {
-    FetchPending = $.ajax({ url: baseurl + 'station/near/'+x+','+y+'?n=10&airport=true&reporting=true&format=json?token='+AVWX,
+    FetchPending = $.ajax({ url: baseurl + 'station/near/'+x+','+y+'?n=10&airport='+true+'&reporting='+true+'&format=json',
                                     timeout: 5000,
                                     cache: false,
-                                    dataType: 'json' });
+                                    dataType: 'json',
+                                    headers: {
+                                        "Authorization":AVWX
+                                    }});
         FetchPending.done(function(data) {
             document.getElementById("results").innerHTML = data
         })
