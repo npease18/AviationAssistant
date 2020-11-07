c8081fe4e7f1f435f881315ce62645732340a0ed
LAX18 Development <gaminglax18@gmail.com>
2020-11-07 00:32:17 +0000

    Minor changes

--- services.sh
+++ services.sh
@@ -1,2 +1,2 @@
-sudo python battery.py
-sudo python api.py
+sudo python /usr/share/dump1090-mutability/html/battery.py & > /dev/null 2>&1 & 
+sudo python /usr/share/dump1090-mutability/html/api.py & > /dev/null 2>&1 & 
