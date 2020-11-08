d81cfa061617b777e9d6f44337e28ef3a4569a04
Nicholas Pease <me@nicholaspease.com>
2020-11-07 20:34:36 -0500

    Fixes

--- api.py
+++ api.py
@@ -19,8 +19,7 @@ def volume():
 def command():
    if request.method == 'POST':
       json = request.get_json()
-      os.system("cd /usr/share/dump1090-mutability/html")
-      output = os.popen(json['command']).read()
+      output = os.popen("/usr/share/dump1090-mutability/html"+json['command']).read()
       return str(output)
 
 
