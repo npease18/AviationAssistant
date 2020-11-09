fb98655ccea92676e3ce663c6f3d2b8e07830b21
Nicholas Pease <me@nicholaspease.com>
2020-11-08 21:05:56 -0500

    Oof

--- api.py
+++ api.py
@@ -19,8 +19,7 @@ def volume():
 def command():
    if request.method == 'POST':
       json = request.get_json()
-      #output = os.popen("cd /usr/share/dump1090-mutability/html && "+json['command']).read()
-      output = os.popen("cd /home/npease/Documents/Projects/avaitionassistant && "+json['command']).read()
+      output = os.popen("cd /usr/share/dump1090-mutability/html && "+json['command']).read(
       return str(output)
 
 
