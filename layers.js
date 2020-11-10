a930514c5c5dd8bdedc7a365d3974e9b8c75eea1
Nicholas Pease <me@nicholaspease.com>
2020-11-09 19:34:07 -0500

    Added Satellite Maps

--- layers.js
+++ layers.js
@@ -4,115 +4,125 @@
 // Base layers configuration
 
 function createBaseLayers() {
-        var layers = [];
-        var timestamps = [];
-        var online = [];
-        var offline = [];
+    var layers = [];
+    var timestamps = [];
+    var online = [];
+    var offline = [];
 
-	if (ChartBundleLayers) {
-                var chartbundleTypes = {
-                        sec: "Sectional Charts",
-                        tac: "Terminal Area Charts",
-                        wac: "World Aeronautical Charts",
-                        enrl: "IFR Enroute Low Charts",
-                        enra: "IFR Area Charts",
-                        enrh: "IFR Enroute High Charts"
-                };
-
-                for (var type in chartbundleTypes) {
-                        online.push(new ol.layer.Tile({
-                                source: new ol.source.TileWMS({
-                                        url: 'http://wms.chartbundle.com/wms',
-                                        params: {LAYERS: type},
-                                        projection: 'EPSG:3857',
-                                        attributions: 'Tiles courtesy of <a href="http://www.chartbundle.com/">ChartBundle</a>'
-                                }),
-                                name: 'chartbundle_' + type,
-                                title: chartbundleTypes[type],
-                                type: 'base',
-                                group: 'chartbundle'}));
-                }
-        }
-
-        online.push(new ol.layer.Tile({
-                source: new ol.source.OSM(),
-                name: 'osm',
-                title: 'OpenStreetMap',
-                type: 'base',
-        }));
-
-	offline.push(new ol.layer.Tile({
-		source: new ol.source.XYZ({
-			url: '/dump1090/tiles/offline-maps/{z}/{x}/{y}.png'
-			}),
-		name: 'offline',
-		title: 'Offline Maps',
-		type: 'base'
-	}));
-
-       var nexrad = new ol.layer.Tile({
-                name: 'nexrad',
-                title: 'NEXRAD',
-                type: 'overlay',
-                opacity: 0.5,
-                visible: false
-        });
-        online.push(nexrad);
-
-        var refreshNexrad = function() {
-                // re-build the source to force a refresh of the nexrad tiles
-                var now = new Date().getTime();
-                nexrad.setSource(new ol.source.XYZ({
-                        url : 'http://mesonet{1-3}.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/{z}/{x}/{y}.png?_=' + now,
-                        attributions: 'NEXRAD courtesy of <a href="http://mesonet.agron.iastate.edu/">IEM</a>'
-                }));
+    if (ChartBundleLayers) {
+        var chartbundleTypes = {
+            sec: "Sectional Charts",
+            tac: "Terminal Area Charts",
+            wac: "World Aeronautical Charts",
+            enrl: "IFR Enroute Low Charts",
+            enra: "IFR Area Charts",
+            enrh: "IFR Enroute High Charts"
         };
 
-        refreshNexrad();
-        window.setInterval(refreshNexrad, 5 * 60000);
-
-
-        var radar = new ol.layer.Tile({
-                name: 'Radar',
-                title: 'Radar',
-                type: 'overlay',
-        });
-
-        online.push(radar);
-
-        var refreshRadar = function() {
-                var apiRequest = new XMLHttpRequest();
-                apiRequest.open("GET", "https://api.rainviewer.com/public/maps.json", true);
-                apiRequest.onload = function(e) {
-            
-                    // save available timestamps and show the latest frame: "-1" means "timestamp.lenght - 1"
-                    timestamps = JSON.parse(apiRequest.response);
-                    
-                    radar.setSource(new ol.source.XYZ({
-                        url : 'https://tilecache.rainviewer.com/v2/radar/' + timestamps[timestamps.length - 1] + '/256/{z}/{x}/{y}/2/1_1.png',
-                }));
-                };
-                apiRequest.send();
+        for (var type in chartbundleTypes) {
+            online.push(new ol.layer.Tile({
+                source: new ol.source.TileWMS({
+                    url: 'http://wms.chartbundle.com/wms',
+                    params: { LAYERS: type },
+                    projection: 'EPSG:3857',
+                    attributions: 'Tiles courtesy of <a href="http://www.chartbundle.com/">ChartBundle</a>'
+                }),
+                name: 'chartbundle_' + type,
+                title: chartbundleTypes[type],
+                type: 'base',
+                group: 'chartbundle'
+            }));
         }
+    }
 
-        refreshRadar();
-        window.setInterval(refreshRadar(), 10 * 60000);
+    online.push(new ol.layer.Tile({
+        source: new ol.source.OSM(),
+        name: 'osm',
+        title: 'OpenStreetMap',
+        type: 'base',
+    }));
 
-        if (online.length > 0) {
-                layers.push(new ol.layer.Group({
-                        name: 'Online Maps',
-                        title: 'Worldwide Online Maps + Overlays',
-                        layers: online
-                }));
-        }
+    online.push(new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            url: 'https://1.aerial.maps.ls.hereapi.com/maptile/2.1/maptile/newest/satellite.day/{z}/{x}/{y}/256/png?apiKey=***REMOVED***'
+        }),
+        name: 'satellite',
+        title: 'Satellite',
+        type: 'base'
+    }));
 
-        if (offline.length > 0) {
-                layers.push(new ol.layer.Group({
-                        name: 'Offline Maps',
-                        title: 'Offline Maps',
-                        layers: offline
-                }));
-        }
+    offline.push(new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            url: '/dump1090/tiles/offline-maps/{z}/{x}/{y}.png'
+        }),
+        name: 'offline',
+        title: 'Offline Maps',
+        type: 'base'
+    }));
 
-        return layers;
+    var nexrad = new ol.layer.Tile({
+        name: 'nexrad',
+        title: 'NEXRAD',
+        type: 'overlay',
+        opacity: 0.5,
+        visible: false
+    });
+    online.push(nexrad);
+
+    var refreshNexrad = function() {
+        // re-build the source to force a refresh of the nexrad tiles
+        var now = new Date().getTime();
+        nexrad.setSource(new ol.source.XYZ({
+            url: 'http://mesonet{1-3}.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/{z}/{x}/{y}.png?_=' + now,
+            attributions: 'NEXRAD courtesy of <a href="http://mesonet.agron.iastate.edu/">IEM</a>'
+        }));
+    };
+
+    refreshNexrad();
+    window.setInterval(refreshNexrad, 5 * 60000);
+
+
+    var radar = new ol.layer.Tile({
+        name: 'Radar',
+        title: 'Radar',
+        type: 'overlay',
+    });
+
+    online.push(radar);
+
+    var refreshRadar = function() {
+        var apiRequest = new XMLHttpRequest();
+        apiRequest.open("GET", "https://api.rainviewer.com/public/maps.json", true);
+        apiRequest.onload = function(e) {
+
+            // save available timestamps and show the latest frame: "-1" means "timestamp.lenght - 1"
+            timestamps = JSON.parse(apiRequest.response);
+
+            radar.setSource(new ol.source.XYZ({
+                url: 'https://tilecache.rainviewer.com/v2/radar/' + timestamps[timestamps.length - 1] + '/256/{z}/{x}/{y}/2/1_1.png',
+            }));
+        };
+        apiRequest.send();
+    }
+
+    refreshRadar();
+    window.setInterval(refreshRadar(), 10 * 60000);
+
+    if (online.length > 0) {
+        layers.push(new ol.layer.Group({
+            name: 'Online Maps',
+            title: 'Worldwide Online Maps + Overlays',
+            layers: online
+        }));
+    }
+
+    if (offline.length > 0) {
+        layers.push(new ol.layer.Group({
+            name: 'Offline Maps',
+            title: 'Offline Maps',
+            layers: offline
+        }));
+    }
+
+    return layers;
 }
