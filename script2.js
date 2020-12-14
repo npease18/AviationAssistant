b003036d1d73da0242f2ff2b2dc4e61fb2b405e3
Nicholas Pease <me@nicholaspease.com>
2020-12-13 19:50:00 -0500

    Settings Page and Home Page Intial Setup

--- script2.js
+++ script2.js
@@ -220,7 +220,12 @@ function goHome() {
     document.getElementById("home_page").style.display = "block"
 }
 
-function openRadar() {
+function goRadar() {
     document.getElementById("radar_page").style.display = "block"
+    document.getElementById("home_page").style.display = "none"
+}
+
+function goSettings() {
+    document.getElementById("settings_page").style.display = "block"
     document.getElementById("home_page").style.display = "none"
 }
