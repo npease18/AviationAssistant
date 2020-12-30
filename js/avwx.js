6d020ae0bbb45b6643875d5b2d0511d7523488aa
LAX18 Development <gaminglax18@gmail.com>
2020-12-30 16:35:15 -0500

    Reverting To Check Compatibility

--- js/avwx.js
+++ js/avwx.js
@@ -2,11 +2,10 @@ var baseurl = "https://avwx.rest/api/"
 
 function nearestStations(x, y) {
     FetchPending = $.ajax({
-        url: baseurl + 'metar/' + y + ',' + x + '?options=info&format=json',
+        url: baseurl + 'metar/' + x + ',' + y + '?token=' + AVWX + '&options=info&format=json',
         timeout: 5000,
         cache: false,
         dataType: 'json',
-        headers: { 'Authorization': AVWX }
     });
     FetchPending.done(function(data) {
         document.getElementById('metar_loading').style.display = "none"
