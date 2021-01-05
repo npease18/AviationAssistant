fb692a0920f55d38476d56ef381ffe6194c62ab6
LAX18 Development <gaminglax18@gmail.com>
2021-01-04 20:50:06 -0500

    Totally working Itinerary tab

--- js/schedules.js
+++ js/schedules.js
@@ -19,6 +19,12 @@ function initializeSchedulesPage() {
 }
 
 function itineraryLoadAirports() {
+    document.getElementById("itinerary_loading").style.display = "block"
+    document.getElementById("arrivals").style.display = "none"
+    document.getElementById("departures").style.display = "none"
+    document.getElementById("sch-arrivals").style.display = "none"
+    document.getElementById("sch-departures").style.display = "none"
+    document.getElementById("itinerary_undefined").style.display = "none"
     while (document.getElementById("itinerary_airports").firstChild) {
         document.getElementById("itinerary_airports").removeChild(document.getElementById("itinerary_airports").firstChild);
     }
@@ -38,6 +44,7 @@ function airportSchedule() {
 }
 
 function retrieveSchedule(airport) {
+    document.getElementById("itinerary_loading").style.display = "none"
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://127.0.0.1:5000/cmd", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -45,15 +52,24 @@ function retrieveSchedule(airport) {
         if (xhr.readyState === 4) {
             var html = xhr.response
             var page_nodes = $($.parseHTML(html));
-            document.getElementById("arrivals").innerHTML = page_nodes.find("#arrivals-board > div").html()
-            document.getElementById("departures").innerHTML = page_nodes.find("#departures-board > div").html()
-            document.getElementById("sch-arrivals").innerHTML = page_nodes.find("#enroute-board > div").html()
-            document.getElementById("sch-departures").innerHTML = page_nodes.find("#scheduled-board > div").html()
-            $("#arrivals > table > thead > tr:nth-child(1) > th > h2").html("Arrivals")
-            $("#departures > table > thead > tr:nth-child(1) > th > h2").html("Departures")
-            $("#sch-arrivals > table > thead > tr:nth-child(1) > th > h2").html("Scheduled Arrivals")
-            $("#sch-departures > table > thead > tr:nth-child(1) > th > h2").html("Scheduled Departures")
-            document.querySelectorAll("#itinerary_page a").forEach(a => a.outerHTML = a.innerHTML);
+            document.getElementById("itinerary_loading").style.display = "none"
+            if (page_nodes.find("#arrivals-board > div").html() === undefined || page_nodes.find("#arrivals-board > div > table > tbody > tr:nth-child(1) > td:nth-child(1)").html() === "&nbsp;") {
+                document.getElementById("itinerary_undefined").style.display = "block"
+            } else {
+                document.getElementById("arrivals").style.display = "inline-table"
+                document.getElementById("departures").style.display = "inline-table"
+                document.getElementById("sch-arrivals").style.display = "inline-table"
+                document.getElementById("sch-departures").style.display = "inline-table"
+                document.getElementById("arrivals").innerHTML = page_nodes.find("#arrivals-board > div").html()
+                document.getElementById("departures").innerHTML = page_nodes.find("#departures-board > div").html()
+                document.getElementById("sch-arrivals").innerHTML = page_nodes.find("#enroute-board > div").html()
+                document.getElementById("sch-departures").innerHTML = page_nodes.find("#scheduled-board > div").html()
+                $("#arrivals > table > thead > tr:nth-child(1) > th > h2").html("Arrivals")
+                $("#departures > table > thead > tr:nth-child(1) > th > h2").html("Departures")
+                $("#sch-arrivals > table > thead > tr:nth-child(1) > th > h2").html("Scheduled Arrivals")
+                $("#sch-departures > table > thead > tr:nth-child(1) > th > h2").html("Scheduled Departures")
+                document.querySelectorAll("#itinerary_page a").forEach(a => a.outerHTML = a.innerHTML);
+            }
         }
     }
     xhr.send(JSON.stringify({
