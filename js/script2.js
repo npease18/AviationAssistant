004d4cf17164aa4cc7066c46b1ef9ebd51ce41a2
Nicholas Pease <me@nicholaspease.com>
2021-01-06 20:23:08 -0500

    Added device information and license terms

--- js/script2.js
+++ js/script2.js
@@ -106,6 +106,34 @@ function volDown() {
     }));
 }
 
+function modal() {
+    // Get the modal
+    var modal = document.getElementById("info_modal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("info_button");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementById("close_modal");
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 function getInitialVolume() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://127.0.0.1:5000/audio", true);
