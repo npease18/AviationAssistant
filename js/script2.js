dd1dab2b78c1b190f2bee72e307768f39dab64fd
Nicholas Pease <me@nicholaspease.com>
2021-01-12 11:45:36 -0500

    Oof

--- js/script2.js
+++ js/script2.js
@@ -151,7 +151,7 @@ function getInitialVolume() {
 
 function getCPUTemp() {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://127.0.0.1:5000/brightness", true);
+    xhr.open("POST", "http://127.0.0.1:5000/cmd", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
