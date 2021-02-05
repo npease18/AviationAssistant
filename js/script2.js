7641b2300094d78dc21ee0f221ee4af18b1609eb
Nicholas Pease <me@nicholaspease.com>
2021-02-04 20:42:21 -0500

    Rudimentary Flights Tab

--- js/script2.js
+++ js/script2.js
@@ -402,3 +402,10 @@ function changeGraph(direction) {
         document.getElementById("graphs_holder").setAttribute("class", "graphs")
     }
 }
+
+
+function toTitleCase(str) {
+    return str.toLowerCase().split(' ').map(function(word) {
+        return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+}
