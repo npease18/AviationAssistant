e3d520463dd88b65f9ec1edf9cb132c543305ae7
Nicholas Pease <me@nicholaspease.com>
2021-02-09 12:15:42 -0500

    Updates!

--- js/script2.js
+++ js/script2.js
@@ -107,26 +107,33 @@ function volDown() {
 }
 
 function modal() {
-    // Get the modal
     var modal = document.getElementById("info_modal");
-
-    // Get the button that opens the modal
     var btn = document.getElementById("info_button");
-
-    // Get the <span> element that closes the modal
     var span = document.getElementById("close_modal");
-
-    // When the user clicks on the button, open the modal
     btn.onclick = function() {
         modal.style.display = "block";
     }
-
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    modal2()
+}
 
-    // When the user clicks anywhere outside of the modal, close it
+function modal2() {
+    var modal = document.getElementById("image_modal");
+    var btn = document.getElementById("img_button");
+    var span = document.getElementById("close_modal2");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
