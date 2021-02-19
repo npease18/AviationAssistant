c6c5b85dceed0a7b5a2d64a3f4856e0e98c88a7d
LAX18 Development <gaminglax18@gmail.com>
2021-02-18 22:43:52 -0500

    Maybe fixed it

--- services.sh
+++ services.sh
@@ -1,5 +1,5 @@
 sudo python /usr/share/dump1090-mutability/html/python/battery.py & > /dev/null 2>&1 & 
 sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
 
-export PORT=5000
+export PORT=6000
 sudo node /home/pi/cors-anywhere/server.js
