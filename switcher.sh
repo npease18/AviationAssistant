f552410c62767120cfe642c1fcc24d0093ab0ed4
Nicholas Pease <me@nicholaspease.com>
2021-02-20 22:21:06 -0500

    Test it

--- switcher.sh
+++ switcher.sh
@@ -6,7 +6,7 @@ then
  sudo systemctl enable dump1090-mutability
  sudo mv /home/pi/.config/Start_Aircraft.desktop /home/pi/.config/autostart/Start_Aircraft.desktop
  sudo mv /home/pi/.config/autostart/Start_Radio.desktop /home/pi/.config/Start_Radio.desktop
- sudo mv /home/pi/Desktop/Files/*.desktop /home/pi/Desktop/
+ sudo mv /home/pi/Desktop/.Files/*.desktop /home/pi/Desktop/
  env DISPLAY=:0.0 pcmanfm -w /usr/share/dump1090-mutability/html/images/logo.png
 
 fi
@@ -19,6 +19,6 @@ then
  sudo mv /home/pi/.config/Start_Radio.desktop /home/pi/.config/autostart/Start_Radio.desktop
  sudo mv /home/pi/.config/autostart/Start_Aircraft.desktop /home/pi/.config/Start_Aircraft.desktop
  env DISPLAY=:0.0 pcmanfm -w /usr/share/dump1090-mutability/html/images/psplogo.png
- sudo mv /home/pi/Desktop/*.desktop /home/pi/Desktop/Files/
+ sudo mv /home/pi/Desktop/*.desktop /home/pi/Desktop/.Files/
 
 fi
