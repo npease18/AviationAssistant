588ffe11a4b99ca77ad6ad8627491622dafab6d4
LAX18 Development <gaminglax18@gmail.com>
2021-07-27 18:47:40 -0400

    Small fixes

--- scripts/internet_data.js
+++ scripts/internet_data.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
 });
 
 function getData() {
-  console.log('http://localhost:7000/data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&bounds='+bounds.lat_north.toFixed(3)+'%2C'+bounds.lat_south.toFixed(3)+'%2C'+bounds.long_east.toFixed(3)+'%2C'+bounds.long_west.toFixed(3)+'&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1')
+  //console.log('http://localhost:7000/data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&bounds='+bounds.lat_north.toFixed(3)+'%2C'+bounds.lat_south.toFixed(3)+'%2C'+bounds.long_east.toFixed(3)+'%2C'+bounds.long_west.toFixed(3)+'&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1')
   const options = {
     url: 'http://localhost:7000/data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&bounds='+bounds.lat_north.toFixed(3)+'%2C'+bounds.lat_south.toFixed(3)+'%2C'+bounds.long_east.toFixed(3)+'%2C'+bounds.long_west.toFixed(3)+'&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1',
     headers: {
@@ -96,12 +96,12 @@ setInterval(function () {
 }, 1000);
 
 app.post("/internet", function(req, res) {
-  console.log(req.body)
+  //console.log(req.body)
   bounds = req.body
   getData()
   return res.send("Recieved")
 })
 
 app.listen(8000, () =>
-console.log(`Example app listening on port 8000!`),
+//console.log(`Example app listening on port 8000!`),
 );
