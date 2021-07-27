0701091e62475724cc3ff6598b3bcae4ca51dcc4
Nicholas Pease <me@nicholaspease.com>
2021-07-26 21:16:40 -0400

    hmm

--- scripts/internet_data.js
+++ scripts/internet_data.js
@@ -85,3 +85,11 @@ setInterval(function () {
 app.post('/internet', (req, res) => {
   return res.send('Received a POST HTTP method');
 });
+
+app.get('/internet', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.listen(8000, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
