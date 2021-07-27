cda44dc1d59f92011bb7ccdc0b328fab06aa01a7
LAX18 Development <gaminglax18@gmail.com>
2021-07-26 22:07:29 -0400

    k

--- js/script2.js
+++ js/script2.js
@@ -126,8 +126,7 @@ function changeMapBounds() {
 
 function getBounds() {
     const extent = OLMap.getView().calculateExtent(OLMap.getSize())
-    point1 = ol.proj.toLonLat(extent[0], extent[1], OLMap.getView().getProjection())
-    point2 = ol.proj.toLonLat(extent[2], extent[3], OLMap.getView().getProjection())
+    point1 = ol.proj.toLonLat(extent, OLMap.getView().getProjection())
     console.log(point1, point2)
     //console.log(extent[2], extent[3])
   }
