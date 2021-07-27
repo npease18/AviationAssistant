21b224fd68805b2fcc3bf81c5818dc911e2f044d
LAX18 Development <gaminglax18@gmail.com>
2021-07-26 21:36:35 -0400

    e

--- scripts/internet_data.js
+++ scripts/internet_data.js
@@ -4,10 +4,11 @@ var json = {}
 const express = require('express')
 const app = express()
 const cors = require('cors');
-app.use(cors(
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-));
-
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 function getData() {
   const options = {
