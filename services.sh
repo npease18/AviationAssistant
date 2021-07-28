1afebee4e1241739e31a4185489565469827b78d
Nicholas Pease <me@nicholaspease.com>
2021-07-27 21:09:59 -0400

    b

--- services.sh
+++ services.sh
@@ -3,3 +3,4 @@ sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1
 
 export PORT=8080
 sudo node /home/pi/cors-anywhere/server.js
+sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js
