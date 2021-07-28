69fbdcc8337c4e224ddb7de5ced3c1ea2939aacf
LAX18 Development <gaminglax18@gmail.com>
2021-07-27 21:24:29 -0400

    c

--- services.sh
+++ services.sh
@@ -1,6 +1,7 @@
 sudo python /usr/share/dump1090-mutability/html/python/battery.py & > /dev/null 2>&1 & 
 sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
+sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js & > /dev/null 2>&1 &
 
 export PORT=8080
 sudo node /home/pi/cors-anywhere/server.js
-sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js & > /dev/null 2>&1 &
+
