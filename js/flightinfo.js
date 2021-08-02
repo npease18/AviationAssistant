6937c11aa303aff2ce9b3630450f69d7f83347cb
LAX18 Development <gaminglax18@gmail.com>
2021-08-02 14:39:24 -0400

    Remove Error Airlines

--- js/flightinfo.js
+++ js/flightinfo.js
@@ -32,18 +32,24 @@ function updateFlightTab() {
                             if (internet_mode_data[element].hex === SelectedPlane) {
                                 var data = internet_mode_data[element]
                                 console.log(internet_mode_data[element])
+                                if (data.airline != "") {
+                                    document.getElementById("flight_flightnum").innerHTML = data.flight.replace(/\D/g, "")
+                                    document.getElementById("flight_status").innerHTML = ""
+                                    document.getElementById("flight_flighticaonum").innerHTML = data.flight
+                                    try {
+                                        document.getElementById("flight_airline").innerHTML = world_airlines[data.airline].nameAirline + " "
+                                    } catch {
+                                        document.getElementById("flight_airline").innerHTML = "N/A "
+                                    }
+                                } else {
+                                    document.getElementById("flight_flightnum").innerHTML = data.flight.replace(/\D/g, "")
+                                    document.getElementById("flight_status").innerHTML = ""
+                                    document.getElementById("flight_flighticaonum").innerHTML = data.flight
+                                    document.getElementById("flight_airline").innerHTML = ""
+                                }
                                 for (airport_search in world_airports) {
-                                    if (world_airports[airport_search].iata === internet_mode_data[element].arr && internet_mode_data[element].arr != "") {
+                                    if (world_airports[airport_search].iata === internet_mode_data[element].arr && internet_mode_data[element].arr != "") {                                       
                                         var airport = world_airports[airport_search]
-                                        document.getElementById("flight_flightnum").innerHTML = data.flight.replace(/\D/g, "")
-                                        document.getElementById("flight_status").innerHTML = ""
-                                        document.getElementById("flight_flighticaonum").innerHTML = data.flight
-                                        try {
-                                            document.getElementById("flight_airline").innerHTML = world_airlines[data.airline].nameAirline + " "
-                                        } catch {
-                                            document.getElementById("flight_airline").innerHTML = "N/A "
-                                        }
-
                                         try {
                                             document.getElementById("flight_airport_long_destination").innerHTML = airport.name
                                             document.getElementById("flight_airport_short_destination").innerHTML = airport.icao
@@ -61,15 +67,6 @@ function updateFlightTab() {
 
                                     if (world_airports[airport_search].iata === internet_mode_data[element].dep && internet_mode_data[element].dep != "") {
                                         var airport = world_airports[airport_search]
-                                        document.getElementById("flight_flightnum").innerHTML = data.flight.replace(/\D/g, "")
-                                        document.getElementById("flight_status").innerHTML = ""
-                                        document.getElementById("flight_flighticaonum").innerHTML = data.flight
-                                        try {
-                                            document.getElementById("flight_airline").innerHTML = world_airlines[data.airline].nameAirline + " "
-                                        } catch {
-                                            document.getElementById("flight_airline").innerHTML = "N/A "
-                                        }
-
                                         try {
                                             document.getElementById("flight_airport_long_origin").innerHTML = airport.name
                                             document.getElementById("flight_airport_short_origin").innerHTML = airport.icao
