2b6658104ba719e6b6bc9aaaad1d36a686efd882
LAX18 Development <gaminglax18@gmail.com>
2021-08-11 19:45:23 -0400

    Making Development Easier

--- services.sh
+++ services.sh
@@ -1,5 +1,9 @@
 sudo python /usr/share/dump1090-mutability/html/python/battery.py & > /dev/null 2>&1 & 
-sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
+
+export FLASKAPP="api.py"
+cd /usr/share/dump1090-mutability/html/python/
+sudo flask run -h 0.0.0.0  & > /dev/null 2>&1 & 
+#sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
 sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js & > /usr/share/dump1090-mutability/html/scripts/log &
 
 export PORT=8080
