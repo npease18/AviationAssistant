7738785a0961d2986c38d6e0d6f86f7a6d65aff6
LAX18 Development <gaminglax18@gmail.com>
2021-08-11 19:11:48 -0400

    Huh

--- js/script2.js
+++ js/script2.js
@@ -214,7 +214,7 @@ function getInitialVolume() {
 
 function getCPUTemp() {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://127.0.0.1:5000/cmd", true);
+    xhr.open("POST", "/cmd", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
