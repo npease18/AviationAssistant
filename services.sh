2d2614d09f20b8c691671ea2fa9832c25eb80c10
Nicholas Pease <me@nicholaspease.com>
2021-08-11 19:55:02 -0400

    Fixes

--- services.sh
+++ services.sh
@@ -1,8 +1,5 @@
 sudo python /usr/share/dump1090-mutability/html/python/battery.py & > /dev/null 2>&1 & 
-
-export FLASK_APP="api.py"
-sudo flask /usr/share/dump1090-mutability/html/python/ run -h 0.0.0.0  & > /dev/null 2>&1 & 
-#sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
+sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
 sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js & > /usr/share/dump1090-mutability/html/scripts/log &
 
 export PORT=8080
