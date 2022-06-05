95aca85bc3eba98f6c7ff207ab212d53ad212bb0
LAX18 Development <gaminglax18@gmail.com>
2022-06-04 22:50:55 -0400

    Shell Script LiveATC update

--- services.sh
+++ services.sh
@@ -4,3 +4,6 @@ sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js & > /dev/
 
 export PORT=8080
 sudo node /home/pi/cors-anywhere/server.js
+
+cd /home/pi/dump1090-info
+wget http://server1.nicholaspease.com:6500/reports/liveatc.json
