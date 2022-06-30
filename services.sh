17566877ff95c1184fd25177fce5229bf0e3ae2b
Nicholas Pease <me@nicholaspease.com>
2022-06-30 02:02:55 -0400

    Updated for HTTPS Server Protocol

--- services.sh
+++ services.sh
@@ -1,7 +1,7 @@
 sudo python /usr/share/dump1090-mutability/html/python/battery.py & > /dev/null 2>&1 & 
 sudo python /usr/share/dump1090-mutability/html/python/api.py & > /dev/null 2>&1 & 
 sudo node /usr/share/dump1090-mutability/html/scripts/internet_data.js & > /dev/null 2>&1 &
-/usr/bin/wget http://server1.nicholaspease.com:6500/reports/liveatc.json -O /home/pi/dump1090-info/liveatc.json
+/usr/bin/wget https://server1.nicholaspease.com:6500/reports/liveatc.json -O /home/pi/dump1090-info/liveatc.json
 
 export PORT=8080
 sudo node /home/pi/cors-anywhere/server.js
